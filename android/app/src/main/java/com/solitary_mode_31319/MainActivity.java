23386ac49efadfd03b01e4f4b6005647c6df91be
Crowdbotics <team@crowdbotics.com>
2021-10-11 16:27:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/solitary_mode_31319/MainActivity.java
@@ -0,0 +1,15 @@
+package com.solitary_mode_31319;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "solitary_mode_31319";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "solitary_mode_31319",
+  url: "https://solitary_mode_31319.botics.co",
+  api: "https://solitary_mode_31319.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
